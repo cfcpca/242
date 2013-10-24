efc45d4da3362955ef3d133938fcc80825f8a7b4
abaumer <austin@austinbaum.com>
2013-10-24 12:32:24 -0700

    add never expires access token for FB api

--- js/events.js
+++ js/events.js
@@ -1,7 +1,7 @@
 (function($){
 
 // temp Variables
-var pageToken = "CAAKmOjQT6EUBALzUZB2ircZAZCUkNll4jeR36mmvCtQps2MZCaWiEothn3bcJOtyVGK2QF2fCDOPPefFyryFdewsPEqliYUjPln2HnKvxY4ELo5xIFqEZCqCHlM9YTAX43BsdEnFOM2mmysEjL9hBjQjBLAwtbog4yhO3hDlCEezDp5ZCxPQag770NE89Mm1wZD";
+var pageToken = "CAAKmOjQT6EUBAHd0oBgWFzNg6zHAVKUDab4HmXF5i3Y9P1jW1SxN6sRaf0ykeUjQC1PcLHuzU6QwptQZBZCKxHkKPDY9gwQuSIpD3F4Y5MEHWoZCm6SmlqV1S3KmHGmtFIt0PVubOifZCZAAZCmPvsZBhedyCx3hpuGhB3wZCpJhrlv6UZA34vjkd";
 var fields = "&fields=picture.type(large),id,name,location,description";
 
 window.fbAsyncInit = function() {
@@ -13,56 +13,38 @@ window.fbAsyncInit = function() {
         xfbml: false // Look for social plugins on the page
     });
 
-    FB.api('/825568655/accounts?access_token='+pageToken, function(response) {
-        if (response) {
-            console.log("received account info");
-            console.log(response);
+    FB.api('/330410753730567/events?access_token=' + pageToken + fields, function(response2) {
+        if (response2) {
+            var entry;
+            console.log("got a response2");
+            console.log(response2);
 
-            for (var i = 0; i < response.data.length; i++) {
-                var fbe = response.data[i];
+            for (var i = 0; i < response2.data.length; i++) {
+                var fbe = response2.data[i],
+                    date = moment(fbe.start_time).format("dddd, MMMM Do YYYY"),
+                    time = moment(fbe.start_time).format("h:mm:ss a");
 
-                // Get info for CFC 242 Page Only
-                if (fbe.id === "330410753730567") {
-                    var newToken = fbe.access_token;
+                entry = "<h2>" + fbe.name + "</h2>" +
+                    "<img src='" + fbe.picture.data.url + "' />" +
+                    "<p><b>" + date + "</b> - " + fbe.location + "</p>" +
+                    "<a href='https://www.facebook.com/events/" + fbe.id + "' title='View " + fbe.name + "' target='_blank'>View " + fbe.name + " on Facebook</a>" +
+                    "<p>" + fbe.description + "</p><hr/>";
 
-                    FB.api('/330410753730567/events?access_token=' + newToken + fields, function(response2) {
-                        if (response2) {
-                            var entry;
-                            console.log("got a response2");
-                            console.log(response2);
+                entry2 = "<div class='media'>"+
+                              "<a class='pull-left' href='https://www.facebook.com/events/" + fbe.id + "' title='View " + fbe.name + "' target='_blank'>"+
+                                "<img class='media-object' src='" + fbe.picture.data.url + "' alt='" + fbe.name + "'>"+
+                              "</a>"+
+                              "<div class='media-body'>"+
+                                "<h2 class='media-heading'>" + fbe.name + "</h2>"+
+                                "<h3 class='lead'>" + date + "</h3>"+
+                                "<p>" + fbe.description + "<br/><a href='https://www.facebook.com/events/" + fbe.id + "' title='View " + fbe.name + "' target='_blank'><span class='glyphicon glyphicon-hand-right'></span> View " + fbe.name + " on Facebook</a></p>"+
+                              "</div>"+
+                            "</div><hr/>";
 
-                            for (var i = 0; i < response2.data.length; i++) {
-                                var fbe = response2.data[i],
-                                    date = moment(fbe.start_time).format("dddd, MMMM Do YYYY"),
-                                    time = moment(fbe.start_time).format("h:mm:ss a");
-
-                                entry = "<h2>" + fbe.name + "</h2>" +
-                                    "<img src='" + fbe.picture.data.url + "' />" +
-                                    "<p><b>" + date + "</b> - " + fbe.location + "</p>" +
-                                    "<a href='https://www.facebook.com/events/" + fbe.id + "' title='View " + fbe.name + "' target='_blank'>View " + fbe.name + " on Facebook</a>" +
-                                    "<p>" + fbe.description + "</p><hr/>";
-
-                                entry2 = "<div class='media'>"+
-                                              "<a class='pull-left' href='https://www.facebook.com/events/" + fbe.id + "' title='View " + fbe.name + "' target='_blank'>"+
-                                                "<img class='media-object' src='" + fbe.picture.data.url + "' alt='" + fbe.name + "'>"+
-                                              "</a>"+
-                                              "<div class='media-body'>"+
-                                                "<h2 class='media-heading'>" + fbe.name + "</h2>"+
-                                                "<h3 class='lead'>" + date + "</h3>"+
-                                                "<p>" + fbe.description + "<br/><a href='https://www.facebook.com/events/" + fbe.id + "' title='View " + fbe.name + "' target='_blank'><span class='glyphicon glyphicon-hand-right'></span> View " + fbe.name + " on Facebook</a></p>"+
-                                              "</div>"+
-                                            "</div><hr/>";
-
-                                $("#fb-events").append(entry2);
-                            };
-                        } else {
-                            console.log("unable to retrieve event information");
-                        }
-                    });
-                }
-            }
+                $("#fb-events").append(entry2);
+            };
         } else {
-            console.log("No FB Link");
+            console.log("unable to retrieve event information");
         }
     });
 };
